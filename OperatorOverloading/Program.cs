be6ab45a516e33221ed759e4796f50d3291032a4
Kinara <91768638+kinarajv@users.noreply.github.com>
2024-02-26 11:15:18 +0700

    Add OperatorOverloading

---
+++ OperatorOverloading/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program 
+{
+	static void Main() 
+	{
+		int a = 1;
+		int b = 2;
+		int result = 1 + 2;
+
+		Car car = new Car(2000);
+		Car car2 = new Car(3000);
+		Car car3 = new Car(4000);
+		Car result = car + car2 + car3 + new Car(5000);
+		Console.WriteLine(car3.price);
+	}
+}
+class Car 
+{
+	public int price;
+	public Car(int price) 
+	{
+		this.price = price;
+	}
+	//Operator Overloading +
+	//static method
+	//return Value => class
+	//Parameter
+	//symbol : / * + - == % < > ++ -- !=
+	public static Car operator +(Car a, Car b) 
+	{
+		return new Car(a.price + b.price);
+	}
+	public static Car operator -(Car a, Car b) 
+	{
+		return new Car(a.price - b.price);
+	}
+}
