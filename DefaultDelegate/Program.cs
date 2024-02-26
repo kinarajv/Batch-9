29380fd85ca7c10676fcdb6cb678ef0441746c64
Kinara <91768638+kinarajv@users.noreply.github.com>
2024-02-26 08:40:08 +0700

    Add Action and Func

---
+++ DefaultDelegate/Program.cs
@@ -0,0 +1,36 @@
+﻿//Method => return type = void
+//Param ->  string
+public delegate void MyDelegate(string s);
+
+class Program 
+{
+	static void Main() 
+	{
+		
+		Subscriber subscriber = new();
+		//Custom Delegate
+		MyDelegate del = subscriber.Notification;
+
+		//public delegate void Action<T>(T parameter)
+		Action<string> action = subscriber.Notification;
+		//public delegate void Action<T,T1>(T param1, T1 param2);
+		Action<int, int> action1 = subscriber.Add;
+
+        Action<string> mydelegate = subscriber.Notification;
+        Action<int, int> mydelegate2 = subscriber.Add;
+
+		action?.Invoke("message");
+		action1?.Invoke(3 , 4);
+	}
+}
+class Subscriber 
+{
+	public void Notification(string data) 
+	{
+		Console.WriteLine(data);
+	}
+	public void Add(int a, int b) 
+	{
+		Console.WriteLine(a + b);
+	}
+}
---
+++ DefaultDelegateWithReturnType/Program.cs
@@ -0,0 +1,33 @@
+﻿//Method => return type = float
+//Param ->  int, int
+public delegate float MyDelegate(int a,int b);
+
+class Program 
+{
+	static void Main() 
+	{
+		
+		Subscriber subscriber = new();
+		//Custom Delegate
+		MyDelegate del = subscriber.Add;
+
+		//public delegate T2 Func<T,T1,T2>(T data1, T1 data2)
+		Func<int, int, float> action = subscriber.Add;
+		Func<int, int, float> action1 = subscriber.Mul;
+		
+
+		action?.Invoke(3,4);
+		action1?.Invoke(3 , 4);
+	}
+}
+class Subscriber 
+{
+	public float Add(int a, int b) 
+	{
+		return (a + b);
+	}
+	public float Mul(int a, int b) 
+	{
+		return (a + b);
+	}
+}
